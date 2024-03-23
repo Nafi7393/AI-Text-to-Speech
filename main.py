f4aacc6ad751c739da43c81e743d10c9a3b0925a
Naich Naznafi <nafi7393@gmail.com>
2024-03-23 14:35:55 +0600

    g

--- main.py
+++ main.py
@@ -21,16 +21,31 @@ def get_all_scripts(csv_file_path='scripts.csv'):
     return data_list
 
 
-def fire_baby():
+def fire_baby(script_path, script_number=1, voice_accent="en_us_006", voice_save_folder="__voice",
+              video_quality=720, background_video_path=None, back_videos_folder_path=None,
+              background_music_path=None, background_music_folder_path="__back_music",
+              gen_voice_volume=2.2, background_music_volume=0.3, sub_font_path='___temps/subtitle.ttf',
+              watermark=True, watermark_text="Money Magnet Mindset", image_fps=40,
+              watermark_font_path='___temps/watermark.ttf', watermark_text_color=(255, 255, 255),
+              watermark_stroke_color=None, watermark_stroke_width=0, watermark_back_react=True,
+              watermark_rect_transparency=120, watermark_rect_stroke=False, watermark_fps=1,
+              watermark_back_rect_color=(255, 255, 255), watermark_back_rect_outline_color=(255, 174, 0),
+              output_location="test"):
     limit = 0
     all_task = []
 
-    details_list = get_all_scripts(csv_file_path="scripts.csv")
+    details_list = get_all_scripts(csv_file_path=script_path)
     num_video = 1
     for title, script in details_list:
         limit += 1
         thread = threading.Thread(target=video_with_back_videos, kwargs={'script': script,
+                                                                         'voice_accent': voice_accent,
+                                                                         'video_quality': video_quality,
+                                                                         'watermark_text': watermark_text,
                                                                          'script_number': num_video,
+                                                                         'back_videos_folder_path': back_videos_folder_path,
+                                                                         'background_music_folder_path': background_music_folder_path,
+                                                                         'background_music_volume': background_music_volume,
                                                                          'output_location': f"OUTPUT/{title}"})
         thread.start()
         all_task.append(thread)
